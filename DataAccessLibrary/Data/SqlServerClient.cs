e34662febb4ab323dd7ba1d8d8826fa3f8a283cd
Ammar Malik <dev.malik.ammar@gmail.com>
2026-03-11 12:49:50 -0400

    Create a Structure of the application

---
+++ DataAccessLibrary/Data/SqlServerClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Data
+{
+    internal class SqlServerClient
+    {
+    }
+}
---
+++ DataAccessLibrary/Logging/LoggingService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Logging
+{
+    internal class LoggingService
+    {
+    }
+}
